f6de31eb15122aa4db6b8c3f1b9c25c3cf064eac
PokazannikovMS <casper-man@yandex.ru>
2022-06-17 18:53:48 +0300

    2022-06-17_18-53

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, render_template_string
 
 
 app = Flask(__name__)
 
+p_title = {'index':'Главная страница'}
+
+
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return 'Hello, World!'
+    context = {}
+    context['title'] = p_title[index.__name__]
+    context['content'] = 'Содержимое страницы'
+    context['menu'] = p_title
+    return render_template('index.html', context=context)
 
 
 if __name__ == '__main__':
